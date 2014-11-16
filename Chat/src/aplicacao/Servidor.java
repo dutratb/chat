52d995dcd45897bb907e18c867ffd2af9a946696
Thiago Brasil <dutratb@gmail.com>
2014-11-15 21:53:04 -0300

    Servidor recebe clientes

--- Chat/src/aplicacao/Servidor.java
+++ Chat/src/aplicacao/Servidor.java
@@ -2,17 +2,22 @@ package aplicacao;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
 	private static final int PORT = 12345;
 	
 	public Servidor() {
 		System.out.println("Inicializando o servidor...");
-		
 		try {
 			ServerSocket servidor = new ServerSocket(PORT);
 			System.out.println("Servidor rodando na porta "
 					+ PORT);
+			while (true) {
+				Socket cliente = servidor.accept();
+				System.out.println("Nova conexão com o cliente "
+						+ cliente.getInetAddress().getHostAddress());
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
