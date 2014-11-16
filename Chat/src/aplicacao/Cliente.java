1a2a7478829d96087f9af7ecd33f600b2e1608f2
Thiago Brasil <dutratb@gmail.com>
2014-11-15 23:00:23 -0300

    Adiciona método entrar ao Cliente

--- Chat/src/aplicacao/Cliente.java
+++ Chat/src/aplicacao/Cliente.java
@@ -1,31 +1,69 @@
 package aplicacao;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
 import thread.RecebeMensagem;
+import util.Acao;
+import util.Mensagem;
 
 public class Cliente {
 	private InetAddress servidorIP;
 	private static final int SERVIDOR_PORTA = 12345;
 	private Socket serverSocket;
 	private DataInputStream input;
-	
+	private ObjectOutputStream output;
+
 	public Cliente() {
 		try {
 			this.servidorIP = InetAddress.getLocalHost();
 			this.serverSocket = new Socket(servidorIP, SERVIDOR_PORTA);
 			System.out.println("Conexão com o Servidor estabelecida!");
 			this.input = new DataInputStream(serverSocket.getInputStream());
+			this.output = new ObjectOutputStream(serverSocket.getOutputStream());
 			new Thread(new RecebeMensagem(input)).start();
+			run();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
-	
+
+	public void run() {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(
+				System.in));
+		String entrada = null;
+		while (true) {
+			try {
+				entrada = teclado.readLine();
+				if (entrada.contains(Acao.entrar.toString())) {
+					// Enquanto não for informado o nome do usuário não aceita
+					while (entrada.replace(Acao.entrar.toString(), "").equals(
+							"")) {
+						System.out.print("Chat:> Erro: Usuário em branco");
+						entrada = teclado.readLine();
+					}
+					entrar(new Mensagem(Acao.entrar, entrada.replace(Acao.entrar
+							.toString(), "").trim()));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void entrar(Mensagem mensagem) throws IOException {
+		enviarMensagem(mensagem);
+	}
+
+	private void enviarMensagem(Mensagem mensagem) throws IOException {
+		this.output.writeObject(mensagem);
+	}
+
 	public static void main(String[] args) {
 		new Cliente();
 	}
